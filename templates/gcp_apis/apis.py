1141c5a1937d1542fa4ad830b61d3637f0eb7aef
vilvamani.arumugam <vilvamani.arumugam@accenture.com>
2020-10-27 09:17:38 +0530

    added enable api py

--- templates/gcp_apis/apis.py
+++ templates/gcp_apis/apis.py
@@ -1,29 +1,22 @@
 # Copyright 2018 Google Inc. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-""" This template creates a network, optionally with subnetworks. """
+
+""" This template enable GCP APIs. """
 
 def generate_config(context):
     """ Entry point for the deployment resources. """
 
     resources = []
-    for i, api in enumerate(context.properties['apis'], 20):
+    for api in context.properties['apis']:
 
         resources.append(
             {
                 'name': api['name'],
                 'action': 'gcp-types/servicemanagement-v1:servicemanagement.services.enable',
-                'properties': api
+                'properties': 
+                    {
+                        'consumerId': context.properties['consumerId'],
+                        'serviceName': api['serviceName']
+                    }
             }
         )
 
