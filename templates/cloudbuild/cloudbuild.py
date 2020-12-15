06ef77d7b4a448f1462368b61f51a1bd9e1bb4df
vilvamani.arumugam <vilvamani007@gmail.com>
2020-12-15 10:50:10 +0530

    added cloudbuild file

---
+++ templates/cloudbuild/cloudbuild.py
@@ -0,0 +1,29 @@
+"""Creates a Cloud Container Builder build."""
+
+import json
+
+
+def GenerateConfig(context):
+  """Generate YAML resource configuration."""
+  
+  name = propc.get(context.env['name'], '-cloudbuild')
+
+  resources = [{
+      'name': name,
+      'action': 'gcp-types/cloudbuild-v1:cloudbuild.projects.builds.create',
+      'metadata': {
+          'runtimePolicy': ['UPDATE_ALWAYS']
+      },
+      'properties': {
+          'steps': [
+              {
+                  'id': 'git_clone',
+                  'name': 'gcr.io/cloud-builders/git',
+                  'args': ['clone', '-b', 'develop', 'https://github.com/vilvamani/gcp-deployment-manager.git', 'quick_start']
+              }
+          ],
+          'timeout': '120s'
+      }
+  }]
+  
+  return { 'resources': resources }
