f32b596742af8ca8eb082c09c3776f15ab34982f
vilvamani.arumugam <vilvamani007@gmail.com>
2020-12-15 11:13:46 +0530

    added cloudbuild file

--- templates/cloudbuild/cloudbuild.py
+++ templates/cloudbuild/cloudbuild.py
@@ -15,14 +15,24 @@ def GenerateConfig(context):
           'runtimePolicy': ['UPDATE_ALWAYS']
       },
       'properties': {
+          'timeout': '120s',
+          'substitutions': {
+              '_HELM_VERSION': '3.2.0'
+          },
           'steps': [
               {
                   'id': 'git_clone',
                   'name': 'gcr.io/cloud-builders/git',
                   'args': ['clone', '-b', 'develop', 'https://github.com/vilvamani/gcp-deployment-manager.git', 'quick_start']
+              },
+              {
+                  'id': 'build_image',
+                  'name': 'gcr.io/cloud-builders/docker',
+                  'args': ['build', '--tag=gcr.io/$PROJECT_ID/helm:${_HELM_VERSION}', '--tag=gcr.io/$PROJECT_ID/helm:latest', '--build-arg', 'HELM_VERSION=v${_HELM_VERSION}', '.'],
+                  'dir': 'quick_start/kubernetes',
+                  'waitFor': ['git_clone']
               }
-          ],
-          'timeout': '120s'
+          ]
       }
   }]
   
