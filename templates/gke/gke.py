8c059a4b664608d2aebeb4bbb2ba9f5bbc5d1821
vilvamani.arumugam <vilvamani007@gmail.com>
2020-12-23 09:20:10 +0530

    Updated network

--- templates/gke/gke.py
+++ templates/gke/gke.py
@@ -2,73 +2,62 @@
 
 """ This template creates a Google Kubernetes Engine cluster. """
 
+
 def generate_config(context):
     """ Entry point for the deployment resources. """
 
     resources = []
     outputs = []
-    project_id = context.env['project']
     properties = context.properties
-    cluster_type = properties.get('clusterLocationType')
+    name = properties['cluster'].get('name', context.env['name'])
+    project_id = properties.get('project', context.env['project'])
     propc = properties['cluster']
-    name = propc.get('name', context.env['name'])
     gke_cluster = {
-        'name': name,
+        'name': context.env['name'],
         'type': '',
         'properties':
             {
+                'parent': 'projects/{}/locations/{}'.format(
+                    project_id,
+                    properties.get('zone', properties.get(
+                        'location', properties.get('region')))
+                ),
                 'cluster':
                     {
-                        'name':
-                            name,
-                        'initialClusterVersion':
-                            propc.get('initialClusterVersion')
-                    }
-            }
+                        'name': name,
+                }
+        }
     }
 
-    if cluster_type == 'Regional':
-        provider = 'gcp-types/container-v1beta1:projects.locations.clusters'
-        if not properties.get('region'):
-            raise KeyError(
-                "region is a required property for a {} Cluster.".
-                format(cluster_type)
-            )
-        parent = 'projects/{}/locations/{}'.format(
-            project_id,
-            properties.get('region')
-        )
-        gke_cluster['properties']['parent'] = parent
-
-    elif cluster_type == 'Zonal':
-        provider = 'container.v1.cluster'
-        if not properties.get('zone'):
-            raise KeyError(
-                "zone is a required property for a {} Cluster.".
-                format(cluster_type)
-            )
+    if properties.get('zone'):
+        # https://cloud.google.com/kubernetes-engine/docs/reference/rest/v1beta1/projects.zones.clusters
+        gke_cluster['type'] = 'gcp-types/container-v1beta1:projects.zones.clusters'
+        # TODO: remove, this is a bug
         gke_cluster['properties']['zone'] = properties.get('zone')
-
-    gke_cluster['type'] = provider
-
-    cluster_props = gke_cluster['properties']['cluster']
+    else:
+        # https://cloud.google.com/kubernetes-engine/docs/reference/rest/v1beta1/projects.locations.clusters
+        gke_cluster['type'] = 'gcp-types/container-v1beta1:projects.locations.clusters'
 
     req_props = ['network', 'subnetwork']
 
-    for prop in req_props:
-        cluster_props[prop] = propc.get(prop)
-        if prop not in propc:
-            raise KeyError(
-                "{} is a required cluster property for a {} Cluster.".format(
-                    prop,
-                    cluster_type
-                )
-            )
-
-    # optional properties
     optional_props = [
+        'initialNodeCount',
+        'initialClusterVersion',
         'description',
+        'nodeConfig',
         'nodePools',
+        'privateClusterConfig',
+        'binaryAuthorization',
+        'binaryAuthorization',
+        'networkConfig',
+        'defaultMaxPodsConstraint',
+        'resourceUsageExportConfig',
+        'authenticatorGroupsConfig',
+        'verticalPodAutoscaling',
+        'tierSettings',
+        'enableTpu',
+        'databaseEncryption',
+        'workloadIdentityConfig',
         'masterAuth',
         'loggingService',
         'monitoringService',
@@ -83,13 +72,22 @@ def generate_config(context):
         'ipAllocationPolicy',
         'masterAuthorizedNetworksConfig',
         'maintenancePolicy',
-        'binaryAuthorization',
         'podSecurityPolicyConfig',
-        'autoscaling',
-        'privateClusterConfig',
-        'verticalPodAutoScaling',
-        'defaultMaxPodsConstraint'
+        'privateCluster',
+        'masterIpv4CidrBlock',
+        'releaseChannel'
     ]
+
+    cluster_props = gke_cluster['properties']['cluster']
+
+    for prop in req_props:
+        cluster_props[prop] = propc.get(prop)
+        if prop not in propc:
+            raise KeyError(
+                "{} is a required cluster property for a {} Cluster."
+                .format(prop,
+                        cluster_type)
+            )
 
     for oprop in optional_props:
         if oprop in propc:
@@ -99,26 +97,37 @@ def generate_config(context):
 
     # Output variables
     output_props = [
-        'name',
         'selfLink',
         'endpoint',
+        'instanceGroupUrls',
+        'clusterCaCertificate',
         'currentMasterVersion',
-        'servicesIpv4Cidr',
-        'instanceGroupUrls'
+        'currentNodeVersion',
+        'servicesIpv4Cidr'
     ]
+
+    if (
+        # https://github.com/GoogleCloudPlatform/deploymentmanager-samples/issues/463
+        propc.get('enableDefaultAuthOutput', False) and (
+            propc.get('masterAuth', {}).get('clientCertificateConfig', False)
+        )
+    ):
+        output_props.append('clientCertificate')
+        output_props.append('clientKey')
 
     for outprop in output_props:
         output_obj = {}
         output_obj['name'] = outprop
-        if outprop == 'instanceGroupUrls':
-            for index, _ in enumerate(propc['nodePools']):
-                output_obj['value'] = '$(ref.{}.nodePools[{}].{})'.format(
-                    name,
-                    str(index),
-                    outprop
-                )
+        ma_props = ['clusterCaCertificate', 'clientCertificate', 'clientKey']
+        if outprop in ma_props:
+            output_obj['value'] = '$(ref.' + context.env['name'] + \
+                                  '.masterAuth.' + outprop + ')'
+        elif outprop == 'instanceGroupUrls':
+            output_obj['value'] = '$(ref.' + context.env['name'] + \
+                '.nodePools[0].' + outprop + ')'
         else:
-            output_obj['value'] = '$(ref.{}.{})'.format(name, outprop)
+            output_obj['value'] = '$(ref.' + \
+                context.env['name'] + '.' + outprop + ')'
 
         outputs.append(output_obj)
 
